d5787c610f2510ace8af559d0a051d6fc8af3a60
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-02-15 02:01:59 +0530

    Create Loader.tsx

---
+++ components/common/Loader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loader = () => {
+    return (
+        <div className='w-full h-screen bg-[#212121] flex items-center justify-center'>
+            <div className="loader"></div>
+        </div>
+    )
+}
+
+export default Loader
