96f3135257fc7f46f4508426539a43e526a5002b
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-02-17 18:01:28 +0530

    update account page

--- pages/account.tsx
+++ pages/account.tsx
@@ -43,7 +43,7 @@ export default function Account() {
     const { setActiveTab, userData, authenticated } = useContext(UserContext);
     const [isEditing, setIsEditing] = useState(false);
 
-    const profileLink = `${userData?.profile}.png`;
+    const profileLink = `${userData?.profile}`;
     const formattedDate = userData?.dob?.split("T")[0];
 
     useEffect(() => {
@@ -75,7 +75,7 @@ export default function Account() {
 
     const { setValue, reset } = form;
     useEffect(() => {
-        if(!userData?.profile) {
+        if (!userData?.profile) {
             setValue("profile", publicId);
         }
     }, [publicId, userData]);
@@ -433,22 +433,93 @@ export default function Account() {
                                 </div>
                             ) : (
                                 <div className='w-full h-auto flex flex-row gap-10 items-start justify-start'>
-                                    <Image
-                                        src={`${process.env.NEXT_PUBLIC_CLOUDINARY_IMAGE_URL}${userData?.profile}.png`}
-                                        alt='profile'
-                                        width={384}
-                                        height={384}
-                                        priority={true}
-                                        className='w-96 h-96 object-cover border-4 border-white shadow'
-                                    />
-                                    <div className='flex flex-col w-full'>
-                                        <div className='flex w-full flex-row items-center justify-between mb-4'>
+                                    <div className='flex flex-col gap-4 min-w-96'>
+                                        <Image
+                                            src={`${process.env.NEXT_PUBLIC_CLOUDINARY_IMAGE_URL}${userData?.profile}.png`}
+                                            alt='profile'
+                                            width={384}
+                                            height={384}
+                                            priority={true}
+                                            className='w-96 h-96 object-cover border-4 border-white shadow'
+                                        />
+                                        <div className='w-full flex flex-col items-center justify-between gap-4'>
+                                            <div className='flex flex-col gap-2 w-full'>
+                                                <Label className='text-base'>Full Name</Label>
+                                                <Input defaultValue={userData?.fullName} disabled />
+                                            </div>
+
+                                            <div className='flex flex-col gap-2 w-full'>
+                                                <Label className='text-base'>Phone Number</Label>
+                                                <Input defaultValue={userData?.phone} className='capitalize' disabled />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className='flex flex-col w-full gap-4'>
+                                        <div className='flex w-full flex-row items-center justify-between mb-2'>
                                             <h2 className='md:text-4xl text-xl font-bold text-[#353535]'>Account Details</h2>
                                             <PenBoxIcon onClick={() => setIsEditing(!isEditing)} className='cursor-pointer' />
                                         </div>
-                                        <p className='text-base text-black text-justify'>
-                                            {userData?.email}
-                                        </p>
+                                        <div className='w-full flex flex-row flex-wrap items-center justify-between md:gap-0 gap-4'>
+                                            <div className='flex flex-col gap-2 md:w-[49%] w-full'>
+                                                <Label className='text-base'>Email</Label>
+                                                <Input defaultValue={userData?.email} disabled />
+                                            </div>
+
+                                            <div className='flex flex-col gap-2 md:w-[49%] w-full'>
+                                                <Label className='text-base'>User Type</Label>
+                                                <Input defaultValue={userData?.userType} className='capitalize' disabled />
+                                            </div>
+                                        </div>
+                                        <div className='w-full flex flex-row flex-wrap items-center justify-between md:gap-0 gap-4'>
+                                            <div className='flex flex-col gap-2 md:w-[49%] w-full'>
+                                                <Label className='text-base'>Full Name</Label>
+                                                <Input defaultValue={userData?.fullName} disabled />
+                                            </div>
+
+                                            <div className='flex flex-col gap-2 md:w-[49%] w-full'>
+                                                <Label className='text-base'>Phone Number</Label>
+                                                <Input defaultValue={userData?.phone} className='capitalize' disabled />
+                                            </div>
+                                        </div>
+                                        <div className='w-full flex flex-row flex-wrap items-center justify-between md:gap-0 gap-4'>
+                                            <div className='flex flex-col gap-2 md:w-[49%] w-full'>
+                                                <Label className='text-base'>Gender</Label>
+                                                <Input defaultValue={userData?.gender} disabled />
+                                            </div>
+
+                                            <div className='flex flex-col gap-2 md:w-[49%] w-full'>
+                                                <Label className='text-base'>Date of Birth</Label>
+                                                <Input defaultValue={formattedDate} className='capitalize' disabled />
+                                            </div>
+                                        </div>
+                                        <div className='w-full flex flex-row flex-wrap items-center justify-between md:gap-0 gap-4'>
+                                            <div className='flex flex-col gap-2 md:w-[49%] w-full'>
+                                                <Label className='text-base'>Joining Year</Label>
+                                                <Input defaultValue={userData?.joiningYear} disabled />
+                                            </div>
+
+                                            <div className='flex flex-col gap-2 md:w-[49%] w-full'>
+                                                <Label className='text-base'>Passing Year</Label>
+                                                <Input defaultValue={userData?.passingYear} className='capitalize' disabled />
+                                            </div>
+                                        </div>
+                                        <div className='w-full flex flex-row flex-wrap items-center justify-between md:gap-0 gap-4'>
+                                            <div className='flex flex-col gap-2 md:w-[49%] w-full'>
+                                                <Label className='text-base'>Account Type</Label>
+                                                <Input defaultValue={userData?.accountType} disabled />
+                                            </div>
+
+                                            <div className='flex flex-col gap-2 md:w-[49%] w-full'>
+                                                <Label className='text-base'>Position</Label>
+                                                <Input defaultValue={userData?.position} className='capitalize' disabled />
+                                            </div>
+                                        </div>
+                                        <div className='w-full flex flex-row flex-wrap items-center justify-between md:gap-0 gap-4'>
+                                            <div className='flex flex-col gap-2 w-full'>
+                                                <Label className='text-base'>Bio</Label>
+                                                <Textarea defaultValue={userData?.bio} disabled />
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             )}
@@ -463,3 +534,18 @@ export default function Account() {
         </div>
     );
 }
+
+// interface AlumniData {
+//     email: string;
+//     phone: string;
+//     fullName: string;
+//     profile: string;
+//     gender: string;
+//     bio: string;
+//     dob: string;
+//     joiningYear: string;
+//     passingYear: string;
+//     userType: string;
+//     accountType: string;
+//     position: string;
+// }
