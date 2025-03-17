1ad9ea239bc1b3744cb6a6ff1750d605fd1f7898
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-03-18 00:35:45 +0530

    new pages added

---
+++ src/academic/components/HeaderAcademic.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import React from 'react'
+
+const HeaderAcademic = () => {
+    return (
+        <div className='w-full h-auto flex items-center justify-center relative'>
+            <Image src={'/bg/header-vcs.png'} alt='page header vcs' width={550} height={550} priority={true} className='w-full h-64 object-cover' />
+            <div className='absolute flex flex-col gap-2 items-center justify-center text-black text-center'>
+                <h1 className='md:text-4xl text-3xl font-bold text-[#353535]'>Coming Soon</h1>
+                <div className='text-lg font-medium'>Academic Records will be coming soon.</div>
+            </div>
+        </div>
+    )
+}
+
+export default HeaderAcademic
---
+++ src/resources/views/ResourcesView.tsx
@@ -0,0 +1,25 @@
+import React, { useContext, useEffect } from 'react';
+import { UserContext } from '@/context/userContext';
+
+import JoinUs from '@/components/common/JoinUs';
+import HeaderResources from '../components/HeaderResources';
+
+const ResourcesView = () => {
+  const { setActiveTab } = useContext(UserContext);
+
+  useEffect(() => {
+    setActiveTab('academic');
+  }, [setActiveTab]);
+
+  return (
+    <div className='w-full h-auto min-h-screen bg-white flex flex-col gap-6 items-center justify-center border-b border-b-[#acacac]'>
+
+      <HeaderResources />
+
+      <JoinUs />
+
+    </div>
+  )
+}
+
+export default ResourcesView;
