223b167c181e4d2aaaa1bb6903df24c221825df2
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-03-19 22:22:58 +0530

    update stories page

---
+++ components/assets/stories.ts
@@ -0,0 +1,22 @@
+export const stories = [
+    {
+        image: "/connect/join-us.jpg",
+        title: "From Classroom to Global Impact: The Journey of Rahul Sharma",
+        link: "/success-stories/rahul-sharma",
+        about: [
+            "Rahul Sharma, a proud alumnus of Vaishali Central School, always had a vision of making a difference in the world. After completing his studies, he pursued a career in environmental sustainability, working with global organizations to combat climate change.",
+            "Rahul’s journey is a testament to how Vaishali Central School nurtures not just students but future leaders. His success is a reflection of the school's commitment to excellence and social responsibility. 🚀🌍",
+            ""
+        ]
+    },
+    {
+        image: "/connect/join-us.jpg",
+        title: "From Classroom to Global Impact: The Journey of Rahul Sharma",
+        link: "/success-stories/rahul-sharma",
+        about: [
+            "Rahul Sharma, a proud alumnus of Vaishali Central School, always had a vision of making a difference in the world. After completing his studies, he pursued a career in environmental sustainability, working with global organizations to combat climate change.",
+            "Rahul’s journey is a testament to how Vaishali Central School nurtures not just students but future leaders. His success is a reflection of the school's commitment to excellence and social responsibility. 🚀🌍",
+            ""
+        ]
+    },
+];
