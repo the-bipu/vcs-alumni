445ebb3c8bf978515b9267dba159b1d0f43a0371
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-03-21 23:32:17 +0530

    update gallery

--- src/gallery/components/GalleryMain/GalleryMain.tsx
+++ src/gallery/components/GalleryMain/GalleryMain.tsx
@@ -12,12 +12,12 @@ const GalleryMain = () => {
                     </p>
                 </div>
                 <div className='w-full h-auto flex flex-row flex-wrap items-center justify-between gap-8'>
-                    <Image src={'/gallery/gallery1.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-96 h-80 object-cover rounded-md shadow-md' />
-                    <Image src={'/gallery/gallery2.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-96 h-80 object-cover rounded-md shadow-md' />
-                    <Image src={'/gallery/gallery3.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-96 h-80 object-cover rounded-md shadow-md' />
-                    <Image src={'/gallery/gallery4.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-96 h-80 object-cover rounded-md shadow-md' />
-                    <Image src={'/gallery/gallery5.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-96 h-80 object-cover rounded-md shadow-md' />
-                    <Image src={'/gallery/gallery6.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-96 h-80 object-cover rounded-md shadow-md' />
+                    <Image src={'/gallery/gallery1.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-96 h-80 object-cover rounded-md shadow-md cursor-pointer transition-all hover:scale-105' />
+                    <Image src={'/gallery/gallery2.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-96 h-80 object-cover rounded-md shadow-md cursor-pointer transition-all hover:scale-105' />
+                    <Image src={'/gallery/gallery3.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-96 h-80 object-cover rounded-md shadow-md cursor-pointer transition-all hover:scale-105' />
+                    <Image src={'/gallery/gallery4.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-96 h-80 object-cover rounded-md shadow-md cursor-pointer transition-all hover:scale-105' />
+                    <Image src={'/gallery/gallery5.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-96 h-80 object-cover rounded-md shadow-md cursor-pointer transition-all hover:scale-105' />
+                    <Image src={'/gallery/gallery6.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-96 h-80 object-cover rounded-md shadow-md cursor-pointer transition-all hover:scale-105' />
                 </div>
             </div>
         </div>
