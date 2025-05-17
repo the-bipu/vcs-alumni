6ff636188c8c379d6a7e76c1b4634d7735105e57
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-05-17 22:54:03 +0530

    update gallery

--- src/gallery/components/MeetUpMain/MeetUpMain.tsx
+++ src/gallery/components/MeetUpMain/MeetUpMain.tsx
@@ -8,13 +8,25 @@ const MeetUpMain = () => {
                 <div className='w-full h-auto flex md:flex-row flex-col md:gap-0 gap-4 items-center justify-between'>
                     <h2 className='md:text-4xl text-2xl md:text-left text-center font-bold text-[#353535]'>VCS Alumni Association Meetups</h2>
                 </div>
-                <div className='w-full h-auto flex flex-row flex-wrap items-center justify-between gap-8'>
-                    <Image src={'/meetups/meetup1.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-96 h-80 object-cover rounded-md shadow-md cursor-pointer transition-all hover:scale-105' />
-                    <Image src={'/meetups/meetup2.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-96 h-80 object-cover rounded-md shadow-md cursor-pointer transition-all hover:scale-105' />
-                    <Image src={'/meetups/meetup3.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-96 h-80 object-cover rounded-md shadow-md cursor-pointer transition-all hover:scale-105' />
-                    <Image src={'/meetups/meetup4.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-96 h-80 object-cover rounded-md shadow-md cursor-pointer transition-all hover:scale-105' />
-                    <Image src={'/meetups/meetup5.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-96 h-80 object-cover rounded-md shadow-md cursor-pointer transition-all hover:scale-105' />
-                    <Image src={'/meetups/meetup6.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-96 h-80 object-cover rounded-md shadow-md cursor-pointer transition-all hover:scale-105' />
+                <div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10'>
+                    <div className='w-full h-64 object-cover rounded-md shadow-md cursor-pointer overflow-hidden'>
+                        <Image src={'/meetups/meetup1.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-full h-full object-cover transition-all hover:scale-105' />
+                    </div>
+                    <div className='w-full h-64 object-cover rounded-md shadow-md cursor-pointer overflow-hidden'>
+                        <Image src={'/meetups/meetup2.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-full h-full object-cover transition-all hover:scale-105' />
+                    </div>
+                    <div className='w-full h-64 object-cover rounded-md shadow-md cursor-pointer overflow-hidden'>
+                        <Image src={'/meetups/meetup3.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-full h-full object-cover transition-all hover:scale-105' />
+                    </div>
+                    <div className='w-full h-64 object-cover rounded-md shadow-md cursor-pointer overflow-hidden'>
+                        <Image src={'/meetups/meetup4.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-full h-full object-cover transition-all hover:scale-105' />
+                    </div>
+                    <div className='w-full h-64 object-cover rounded-md shadow-md cursor-pointer overflow-hidden'>
+                        <Image src={'/meetups/meetup5.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-full h-full object-cover transition-all hover:scale-105' />
+                    </div>
+                    <div className='w-full h-64 object-cover rounded-md shadow-md cursor-pointer overflow-hidden'>
+                        <Image src={'/meetups/meetup6.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-full h-full object-cover transition-all hover:scale-105' />
+                    </div>
                 </div>
             </div>
         </div>
--- src/index/components/GalleryHome/GalleryHome.tsx
+++ src/index/components/GalleryHome/GalleryHome.tsx
@@ -16,13 +16,25 @@ const GalleryHome = () => {
                         </button>
                     </Link>
                 </div>
-                <div className='w-full h-auto flex flex-row flex-wrap items-center justify-between gap-8'>
-                    <Image src={'/gallery/gallery1.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-96 h-80 object-cover rounded-md shadow-md cursor-pointer transition-all hover:scale-105' />
-                    <Image src={'/gallery/gallery2.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-96 h-80 object-cover rounded-md shadow-md cursor-pointer transition-all hover:scale-105' />
-                    <Image src={'/gallery/gallery3.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-96 h-80 object-cover rounded-md shadow-md cursor-pointer transition-all hover:scale-105' />
-                    <Image src={'/gallery/gallery4.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-96 h-80 object-cover rounded-md shadow-md cursor-pointer transition-all hover:scale-105' />
-                    <Image src={'/gallery/gallery5.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-96 h-80 object-cover rounded-md shadow-md cursor-pointer transition-all hover:scale-105' />
-                    <Image src={'/gallery/gallery6.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-96 h-80 object-cover rounded-md shadow-md cursor-pointer transition-all hover:scale-105' />
+                <div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10'>
+                    <div className='w-full h-64 object-cover rounded-md shadow-md cursor-pointer overflow-hidden'>
+                        <Image src={'/gallery/gallery1.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-full h-full object-cover transition-all hover:scale-105' />
+                    </div>
+                    <div className='w-full h-64 object-cover rounded-md shadow-md cursor-pointer overflow-hidden'>
+                        <Image src={'/gallery/gallery2.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-full h-full object-cover transition-all hover:scale-105' />
+                    </div>
+                    <div className='w-full h-64 object-cover rounded-md shadow-md cursor-pointer overflow-hidden'>
+                        <Image src={'/gallery/gallery3.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-full h-full object-cover transition-all hover:scale-105' />
+                    </div>
+                    <div className='w-full h-64 object-cover rounded-md shadow-md cursor-pointer overflow-hidden'>
+                        <Image src={'/gallery/gallery4.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-full h-full object-cover transition-all hover:scale-105' />
+                    </div>
+                    <div className='w-full h-64 object-cover rounded-md shadow-md cursor-pointer overflow-hidden'>
+                        <Image src={'/gallery/gallery5.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-full h-full object-cover transition-all hover:scale-105' />
+                    </div>
+                    <div className='w-full h-64 object-cover rounded-md shadow-md cursor-pointer overflow-hidden'>
+                        <Image src={'/gallery/gallery6.jpg'} alt='sphere' width={550} height={550} priority={true} className='w-full h-full object-cover transition-all hover:scale-105' />
+                    </div>
                 </div>
             </div>
         </div>
